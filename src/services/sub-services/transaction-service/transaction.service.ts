c3c763c604e4be54935879455e94f98cb20d846a
refilwe-m <refilwe.dev@gmail.com>
2023-11-12 03:48:23 +0200

    Response

--- src/services/sub-services/transaction-service/transaction.service.ts
+++ src/services/sub-services/transaction-service/transaction.service.ts
@@ -1,8 +1,11 @@
 import transactionUrls from "./transaction.urls";
 import { closedAPI } from "../../service";
+import { AxiosResponse } from "axios";
 
 export const TransactionService = {
   getTransactions: () => {
-    return closedAPI.get(transactionUrls.getTransactionsUrl());
+    return closedAPI
+      .get(transactionUrls.getTransactionsUrl())
+      .then((apiResponse: AxiosResponse) => apiResponse?.data);
   },
 };
