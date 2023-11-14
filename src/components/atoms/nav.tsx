982f06742a80b0ac0ab6be4100085dd3fa53d523
refilwe-m <refilwe.dev@gmail.com>
2023-11-14 17:12:40 +0200

    Changed nav to side nav

--- src/components/atoms/nav.tsx
+++ src/components/atoms/nav.tsx
@@ -1,5 +1,6 @@
 import { NavLink } from "react-router-dom";
 import { Logo } from "../../assets";
+import { FiHome, FiLogOut, FiTarget, FiUser } from "react-icons/fi";
 
 export const Nav = () => {
   const navLinks = [
@@ -12,7 +13,7 @@ export const Nav = () => {
       path: "/goals",
     },
     {
-      name: "Gugu Mokwena",
+      name: "Profile",
       path: "/profile",
     },
     {
@@ -20,21 +21,31 @@ export const Nav = () => {
       path: "/login",
     },
   ];
+
+  const icons = [
+    <FiHome size={24} />,
+    <FiTarget size={24} />,
+    <FiUser size={24} />,
+    <FiLogOut size={24} />,
+  ];
   return (
-    <nav className="w-full sticky bg-transparent drop-shadow-2xl">
-      <ul className="flex justify-between items-center p-3">
-        <img src={Logo} alt="logo" className="w-12" />
-        <section className="flex justify-between gap-6">
-          {navLinks.map((link) => (
-            <NavLink
-              key={link.name}
-              to={link.path}
-              className={`${link.name
-                .replace(" ", "")
-                .toLowerCase()} text-xs hover:text-blue-300`}
-            >
-              {link.name}
-            </NavLink>
+    <nav className="w-full sticky bg-transparent drop-shadow-2xl max-w-xs">
+      <ul className="flex flex-col p-3">
+        <img src={Logo} alt="logo" className="w-18 self-center" />
+        <section className="flex flex-col justify-between gap-6 ">
+          {navLinks.map((link, index) => (
+            <section className="flex items-center gap-2 text-primary hover:text-blue-300">
+              {icons[index]}
+              <NavLink
+                key={link.name}
+                to={link.path}
+                className={`${link.name
+                  .replace(" ", "")
+                  .toLowerCase()} text-xs `}
+              >
+                {link.name}
+              </NavLink>
+            </section>
           ))}
         </section>
       </ul>
--- src/components/templates/app-layout.tsx
+++ src/components/templates/app-layout.tsx
@@ -3,11 +3,11 @@ import { Nav } from "../atoms";
 
 export const AppLayout = ({ children }: ChildrenProps) => {
   return (
-    <>
+    <main className="flex">
       <Nav />
       <section className="w-screen h-screen pt-6 px-12 bg-app-background bg-cover bg-center">
         {children}
       </section>
-    </>
+    </main>
   );
 };
